d5e0372e4a597adacf530ca99253f23c2f1906ff
pinalh3 <pinalh3@gmail.com>
2023-02-04 11:08:16 -0400

    ejercicio1 rev

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -6,6 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        context.getBean("saludo");
+        Saludo mensaje = (Saludo)context.getBean("saludo");
+        mensaje.imprimirSaludo();
     }
 }
--- src/main/java/com/example/Saludo.java
+++ src/main/java/com/example/Saludo.java
@@ -3,6 +3,10 @@ package com.example;
 public class Saludo {
 
     public Saludo(){
+
+    }
+
+    public void imprimirSaludo(){
         System.out.println("Hola desde Venezuela");
     }
 }
